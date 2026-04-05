0ddf6d05385b2d9142d72f9ae89d834ad2dcacf3
Mike Hinton <mikehinton@Mikes-MBP.lan>
2026-04-05 15:13:07 +0100

    Update site

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { Link, NavLink, useLocation } from 'react-router-dom'
 import './Navbar.css'
 
