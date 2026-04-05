90efe12c65b31355137ad013e75d19ea77cb89e4
Mike Hinton <mikehinton@Mikes-MBP.lan>
2026-04-05 15:36:47 +0100

    Update site

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -210,9 +210,9 @@ export default function Home() {
           <div className="divider center" />
           <div style={{ display: 'flex', justifyContent: 'center', marginTop: 24 }}>
             <iframe
-              src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fmikehintondrivingschool&tabs=timeline&width=340&height=271&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+              src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fmikehintondrivingschool&tabs&width=340&height=130&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
               width="340"
-              height="271"
+              height="130"
               style={{ border: 'none', overflow: 'hidden' }}
               scrolling="no"
               frameBorder="0"
