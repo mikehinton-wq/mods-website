bbc450b5db079641a5bf3f145c86c96e16f7f106
Mike Hinton <mikehinton@Mikes-MBP.lan>
2026-04-06 23:10:08 +0100

    Update site

--- src/pages/BlogConsistency.jsx
+++ src/pages/BlogConsistency.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import { Helmet } from 'react-helmet-async'
 
 const tags = ['Mindset', 'Learning to Drive', 'Driving Lessons', 'Practice', 'Progress', 'Learner Driver', 'Driving Tips']
 
