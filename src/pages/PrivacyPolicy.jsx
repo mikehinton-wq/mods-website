91fc7e8084021a1fef01aa6a8cb28371d522ec0e
Mike Hinton <mikehinton@Mikes-MBP.lan>
2026-04-06 20:02:19 +0100

    Update site

---
+++ src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,167 @@
+import { Helmet } from 'react-helmet-async'
+
+export default function PrivacyPolicy() {
+  return (
+    <>
+      <Helmet>
+        <title>Privacy Policy | Mike Hinton Driving School Rotherham</title>
+        <meta name="description" content="Privacy policy for Mike Hinton Driving School. Learn how we collect, use and protect your personal data in accordance with GDPR." />
+        <link rel="canonical" href="https://www.mikehintondrivingschool.co.uk/privacy-policy" />
+      </Helmet>
+      <section className="page-hero">
+        <div className="container">
+          <span className="badge badge-primary" style={{ marginBottom: 14 }}>Legal</span>
+          <h1>Privacy Policy</h1>
+          <p>How we collect, use and protect your personal information.</p>
+        </div>
+      </section>
+
+      <section className="section">
+        <div className="container" style={{ maxWidth: 820, margin: '0 auto' }}>
+
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 32, fontSize: '1.02rem' }}>
+            We respect the privacy of everyone who visits this website. As a result we would like to inform you regarding the way we would use your personal data. We recommend you to read this privacy policy so that you understand our approach towards the use of your personal data.
+          </p>
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 32, fontSize: '1.02rem' }}>
+            By submitting your personal data to us, you will be treated as having given your permission – where necessary and appropriate – for disclosures referred to in this policy. This policy is compliant with the EU General Data Protection Regulation (GDPR).
+          </p>
+
+          <h2 style={{ marginBottom: 12 }}>What does this privacy policy cover?</h2>
+          <div className="divider" />
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 32 }}>
+            This privacy policy is to inform you regarding the use of your personal information which is collected during your visit to one of our websites. This privacy policy applies to the majority of our websites, however there may be occasions where there is a need to have a slightly different privacy policy. Whenever a website of Mike Hinton Driving School has a different privacy policy it will be made clear that that privacy policy differs from this general Mike Hinton Driving School privacy policy. On your journey around the Mike Hinton Driving School websites please check the privacy policy of each website which you visit and do not assume that this privacy policy applies to all Mike Hinton Driving School.
+          </p>
+
+          <h2 style={{ marginBottom: 12 }}>Who is responsible for the personal information collected on this site?</h2>
+          <div className="divider" />
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 32 }}>
+            Your personal data is controlled ultimately by Mike Hinton Driving School. 15 Elton Lane, Sunnyside, Rotherham, S66 3RU.
+          </p>
+
+          <h2 style={{ marginBottom: 12 }}>Purposes of the collection of your data</h2>
+          <div className="divider" />
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 16 }}>
+            One of the purposes of our website is to inform you of who we are and what we do. We collect and use personal information (including name, address, telephone number and email) to better provide you with the required services, or information. We would therefore use your personal information in order to:
+          </p>
+          <ul style={{ color: 'var(--grey-700)', lineHeight: 2, marginBottom: 24, paddingLeft: 24 }}>
+            <li>respond to queries or requests submitted by you</li>
+            <li>process orders or applications submitted by you</li>
+            <li>administer or otherwise carry out our obligations in relation to any agreement you have with us</li>
+            <li>anticipate and resolve problems with any goods or services supplied to you</li>
+            <li>create products or services that may meet your needs</li>
+          </ul>
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 16 }}>
+            To optimise our services we may wish to use your personal data for direct marketing. As we respect your privacy we will only use your personal data for this purpose when you are aware thereof and if required we will request your consent prior to using your personal data for direct marketing.
+          </p>
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 16 }}>
+            Additionally, if at any time you wish us to stop using your information for any or all of the above purposes, please contact us. We will stop the use of your information for such purposes as soon as it is reasonably possible to do so.
+          </p>
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 16 }}>
+            Except as set out in this privacy policy, we will not disclose any personally identifiable information without your permission unless we are legally entitled or required to do so (for example, if required to do so by legal process or for the purposes of prevention of fraud or other crime) or if we believe that such action is necessary to protect and/or defend our rights, property or personal safety and those of our users/customers or other individuals.
+          </p>
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 32 }}>
+            We will always seek your consent to publish your personal information such as your name and picture following a successful driving test.
+          </p>
+
+          <h2 style={{ marginBottom: 12 }}>Collection of non-personal information</h2>
+          <div className="divider" />
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 32 }}>
+            We may automatically collect non-personal information about you such as the type of internet browsers you use or the website from which you linked to our website. We may also aggregate details which you have submitted to the site. You cannot be identified from this information and it is only used to assist us in providing an effective service on this web site. We may from time to time supply third parties with this non-personal or aggregated data for uses in connection with this website.
+          </p>
+
+          <h2 style={{ marginBottom: 12 }}>Interaction between you and us</h2>
+          <div className="divider" />
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 16 }}>
+            We are interested in your views, and we value feedback from our clients and visitors. If at any time this website offers any chat rooms, notice board facilities, newsgroups etc we may collect the personal information that you disclose. Such information will be used in accordance with this privacy policy.
+          </p>
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 32 }}>
+            We aim to keep our information about you as accurate as possible. If you would like to review, change or delete the details you have supplied us with, please contact us.
+          </p>
+
+          <h2 style={{ marginBottom: 12 }}>The Driving Test and your Personal Data</h2>
+          <div className="divider" />
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 16 }}>
+            Your driving test may be booked by your Approved Driving Instructor (ADI). When they book the test for you they may use their own email address for this purpose which will mean:
+          </p>
+          <ul style={{ color: 'var(--grey-700)', lineHeight: 2, marginBottom: 16, paddingLeft: 24 }}>
+            <li>Your instructor will receive an email confirmation of the booking.</li>
+            <li>They are alerted by email should the test be moved or cancelled by the DVSA.</li>
+          </ul>
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 16 }}>
+            By agreeing to this privacy policy, you are allowing your instructor to receive the following:
+          </p>
+          <ul style={{ color: 'var(--grey-700)', lineHeight: 2, marginBottom: 16, paddingLeft: 24 }}>
+            <li>Any email correspondence from the DVSA related to your driving test.</li>
+            <li>DL25 Driving Test Report electronically after you complete your driving test.</li>
+          </ul>
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 32 }}>
+            If you wish to opt out of this part of your privacy policy, you can do so by informing your Approved Driving Instructor (ADI) of your decision, or by changing the email address at the start of your driving test when you are at the test centre.
+          </p>
+
+          <h2 style={{ marginBottom: 12 }}>Security of your personal data</h2>
+          <div className="divider" />
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 32 }}>
+            We will never sell your data to third parties or any other person. All personal data will be removed from our systems once the products and services purchased have completed their purpose.
+          </p>
+
+          <h2 style={{ marginBottom: 12 }}>Advertisers</h2>
+          <div className="divider" />
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 32 }}>
+            We may use an outside ad company to display ads on our site. These ads may contain cookies. While we may use cookies in other parts of our website, cookies received with banner ads are collected by our ad company, and we do not have access to this information.
+          </p>
+
+          <h2 style={{ marginBottom: 12 }}>External Links</h2>
+          <div className="divider" />
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 32 }}>
+            This site contains links to other sites. Mike Hinton Driving School is not responsible for the privacy practices or the content of such websites.
+          </p>
+
+          <h2 style={{ marginBottom: 12 }}>Contact and Registration Forms</h2>
+          <div className="divider" />
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 32 }}>
+            Our site's contact and registration forms require users to give us contact information (like their name and email address). Information from the contact and registration forms is used to send information about our company.
+          </p>
+
+          <h2 style={{ marginBottom: 12 }}>Cookies Policy</h2>
+          <div className="divider" />
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 16 }}>
+            We use the term "cookies" to refer to cookies and other similar technologies covered by the EU Directive on privacy in electronic communications.
+          </p>
+
+          <h3 style={{ marginBottom: 8, marginTop: 24, color: 'var(--navy)' }}>What is a cookie?</h3>
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 16 }}>
+            Cookies are small data files that your browser places on your computer or device. Cookies help your browser navigate a website and the cookies themselves cannot collect any information stored on your computer or your files. When a server uses a web browser to read cookies they can help a website deliver a more user-friendly service.
+          </p>
+
+          <h3 style={{ marginBottom: 8, marginTop: 24, color: 'var(--navy)' }}>Why do we use cookies?</h3>
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 16 }}>
+            We use cookies to learn more about the way you interact with our content and help us to improve your experience when visiting our website. Cookies remember the type of browser you use and which additional browser software you have installed. They also remember your preferences, such as language and region, which remain as your default settings when you revisit the website.
+          </p>
+
+          <h3 style={{ marginBottom: 8, marginTop: 24, color: 'var(--navy)' }}>How are third party cookies used?</h3>
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 16 }}>
+            For some of the functions within our websites we use third party suppliers, for example, when you visit a page with videos embedded from or links to YouTube. These videos or links (and any other content from third party suppliers) may contain third party cookies and you may wish to consult the policies of these third party websites for information regarding their use of cookies.
+          </p>
+
+          <h3 style={{ marginBottom: 8, marginTop: 24, color: 'var(--navy)' }}>How do I reject and delete cookies?</h3>
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 32 }}>
+            We will not use cookies to collect personally identifiable information about you. However, should you wish to do so, you can choose to reject or block the cookies set by Mike Hinton Driving School or the websites of any third party suppliers by changing your browser settings. Please note that most browsers automatically accept cookies so if you do not wish cookies to be used you may need to actively delete or block the cookies. You can also visit <a href="https://www.allaboutcookies.org" target="_blank" rel="noreferrer" style={{ color: 'var(--primary)' }}>www.allaboutcookies.org</a> for details on how to delete or reject cookies. Note that if you reject the use of cookies you will still be able to visit our websites but some of the functions may not work correctly.
+          </p>
+
+          <h2 style={{ marginBottom: 12 }}>Changes to this policy</h2>
+          <div className="divider" />
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 32 }}>
+            From time to time we may make changes to this privacy policy. If we make any substantial changes to this privacy policy and the way in which we use your personal data we will post these changes on this page and will do our best to notify you of any significant changes. Please check our privacy policy on a regular basis.
+          </p>
+
+          <h2 style={{ marginBottom: 12 }}>How can you contact us?</h2>
+          <div className="divider" />
+          <p style={{ color: 'var(--grey-700)', lineHeight: 1.8, marginBottom: 32 }}>
+            Please contact Mike Hinton Driving School if you would like to know more about our privacy policy. You can reach us at <a href="mailto:mikejhinton@outlook.com" style={{ color: 'var(--primary)' }}>mikejhinton@outlook.com</a> or by calling <a href="tel:07915999994" style={{ color: 'var(--primary)' }}>07915 999994</a>.
+          </p>
+
+        </div>
+      </section>
+    </>
+  )
+}
