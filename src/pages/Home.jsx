1d6b4974f90211db4822d9b78db1b8b67545102c
Mike Hinton <mikehinton@Mikes-MBP.lan>
2026-04-10 09:01:00 +0100

    Update site

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import { Helmet } from 'react-helmet-async'
+import MailchimpSignup from '../components/MailchimpSignup'
 import './Home.css'
 
 const services = [
