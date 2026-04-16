af3483dba4ee8d7759fa21caf36cc16d0614bb22
Mike Hinton <mikehinton@Mikes-MBP.lan>
2026-04-16 21:44:37 +0100

    Update site

--- src/App.jsx
+++ src/App.jsx
@@ -87,6 +87,7 @@ export default function App() {
           <Route path="/blog/consistency-beats-intensity" element={<BlogConsistency />} />
           <Route path="/blog/when-you-are-your-best-self" element={<BlogBestSelf />} />
           <Route path="/blog/how-to-get-rid-of-nerves" element={<BlogGetRidOfNerves />} />
+          <Route path="/blog/is-your-car-safe-to-drive" element={<BlogVehicleSafe />} />
           <Route path="/faqs"                      element={<FAQs />} />
           <Route path="/driving-syllabus"          element={<DrivingSyllabus />} />
           <Route path="/contact"                   element={<Contact />} />
