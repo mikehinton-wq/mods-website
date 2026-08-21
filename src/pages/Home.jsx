cf36df40fb3cee7a78614b291716cd5525b979ed
Mike Hinton <mikehinton@Mikes-MBP.lan>
2026-08-21 08:48:51 +0100

    Update site

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -418,6 +418,25 @@ export default function Home() {
             <div className="elfsight-app-a8fdbcd6-2bb7-42f8-a307-3e274cc31116" data-elfsight-app-lazy></div>
             <div className="elfsight-app-64c02bc2-4726-47d5-8ef3-b5d2c9a9441f" data-elfsight-app-lazy></div>
           </div>
+          <div style={{ textAlign: 'center', marginTop: 32 }}>
+            <p style={{ color: 'var(--grey-700)', marginBottom: 16, fontSize: '0.95rem' }}>Passed your test? We'd love to hear from you!</p>
+            <a
+              href="https://www.facebook.com/mikehintondrivingschool/reviews"
+              target="_blank"
+              rel="noreferrer"
+              style={{
+                display: 'inline-flex', alignItems: 'center', gap: 10,
+                background: '#1877F2', color: '#fff',
+                fontFamily: 'Raleway, sans-serif', fontWeight: 700, fontSize: '1rem',
+                padding: '14px 28px', borderRadius: 50, textDecoration: 'none',
+              }}
+            >
+              <svg width="20" height="20" viewBox="0 0 24 24" fill="white">
+                <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
+              </svg>
+              Leave a Review on Facebook
+            </a>
+          </div>
         </div>
       </section>
 
